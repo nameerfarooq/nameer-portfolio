670b871f3dbafb0ce0492cac0dceaa0e2a063222
Nameerfarooq <nameerfarooquit@gmail.com>
2024-06-05 16:01:55 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <div className="font-sora">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
