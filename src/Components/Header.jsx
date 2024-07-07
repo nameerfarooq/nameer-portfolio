c5bda60a3505cda406c60f8d6e7d8fb7b17ed5f3
Nameerfarooq <nameerfarooquit@gmail.com>
2024-07-07 23:39:34 +0500

    responsive header added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,46 +1,133 @@
 import FancyButton from "./FancyButton";
 import resume from "../assets/Nameer (React Js + MERN).pdf";
-const Header = () => {
+
+import React, { useEffect, useState } from "react";
+import { MotionConfig, motion } from "framer-motion";
+import HeaderDrawer from "./HeaderDrawer";
+
+const AnimatedHamburgerButton = ({ open }) => {
+  const [active, setActive] = useState(open);
+  useEffect(() => {
+    setActive(open);
+  }, [open]);
   return (
-    <div
-      id="top"
-      className="max-w-7xl mx-auto p-3 flex justify-between gap-3 header"
+    <MotionConfig
+      transition={{
+        duration: 0.5,
+        ease: "easeInOut",
+      }}
     >
-      <div className="text-black-700 font-semibold text-xl flex-1 items-center flex">
-        Muhammad Nameer.
+      <motion.button
+        initial={false}
+        animate={active ? "open" : "closed"}
+        onClick={() => setActive((pv) => !pv)}
+        className="relative h-12 w-12 rounded-full bg-black transition-colors hover:bg-black"
+      >
+        <motion.span
+          variants={VARIANTS.top}
+          className="absolute h-1 w-8 bg-white"
+          style={{ y: "-50%", left: "50%", x: "-50%", top: "35%" }}
+        />
+        <motion.span
+          variants={VARIANTS.middle}
+          className="absolute h-1 w-8 bg-white"
+          style={{ left: "50%", x: "-50%", top: "50%", y: "-50%" }}
+        />
+        <motion.span
+          variants={VARIANTS.bottom}
+          className="absolute h-1 w-3 bg-white"
+          style={{
+            x: "-50%",
+            y: "50%",
+            bottom: "35%",
+            left: "calc(50% + 10px)",
+          }}
+        />
+      </motion.button>
+    </MotionConfig>
+  );
+};
+
+const VARIANTS = {
+  top: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      top: ["35%", "50%", "50%"],
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      top: ["50%", "50%", "35%"],
+    },
+  },
+  middle: {
+    open: {
+      rotate: ["0deg", "0deg", "-45deg"],
+    },
+    closed: {
+      rotate: ["-45deg", "0deg", "0deg"],
+    },
+  },
+  bottom: {
+    open: {
+      rotate: ["0deg", "0deg", "45deg"],
+      bottom: ["35%", "50%", "50%"],
+      left: "50%",
+    },
+    closed: {
+      rotate: ["45deg", "0deg", "0deg"],
+      bottom: ["50%", "50%", "35%"],
+      left: "calc(50% + 10px)",
+    },
+  },
+};
+
+const Header = () => {
+  const [open, setopen] = useState(false);
+  const handleOpen = () => {
+    setopen(!open);
+  };
+  return (
+    <>
+      <div
+        id="top"
+        className="max-w-7xl mx-auto p-3 flex justify-between gap-3 header"
+      >
+        <div className="nav-logo text-black-700 font-semibold text-xl flex-1 items-center flex">
+          Muhammad Nameer.
+        </div>
+        <div className="nav-items flex gap-6 items-center flex-1 justify-center font-semibold">
+          <div className=" cursor-pointer">
+            <a href="#skills">Skills</a>
+          </div>
+          <div className=" cursor-pointer">
+            {" "}
+            <a href="#experience"> Experience</a>
+          </div>
+          <div className=" cursor-pointer">
+            {" "}
+            <a href="#work">Work</a>
+          </div>
+          <div className=" cursor-pointer">
+            {" "}
+            <a href="#achievements">Achievements</a>
+          </div>
+          <div className=" cursor-pointer">
+            <a href="#contact">Contact</a>
+          </div>
+        </div>
+        <div className="flex-1 justify-end flex gap-3 items-center">
+          <a href={resume} target="_blank" download>
+            <div className="resumeButton">
+              <FancyButton />
+            </div>
+          </a>
+          <div className="burgerButton" onClick={handleOpen}>
+            <AnimatedHamburgerButton open={open} />
+          </div>
+        </div>
       </div>
-      <div className="nav-items flex gap-6 items-center flex-1 justify-center font-semibold">
-        <div className=" cursor-pointer">
-          <a href="#skills">Skills</a>
-        </div>
-        <div className=" cursor-pointer">
-          {" "}
-          <a href="#experience"> Experience</a>
-        </div>
-        <div className=" cursor-pointer">
-          {" "}
-          <a href="#work">Work</a>
-        </div>
-        <div className=" cursor-pointer">
-          {" "}
-          <a href="#achievements">Achievements</a>
-        </div>
-        <div className=" cursor-pointer">
-          <a href="#contact">Contact</a>
-        </div>
-      </div>
-      <div className="flex-1 justify-end flex">
-        {/* <div className="flex gap-2 items-center w-max font-semibold py-3 px-8 rounded bg-black text-white hover:opacity-75 cursor-pointer">
-          <div> Resume</div>
-          <span>
-            <FaArrowDown />
-          </span>
-        </div> */}
-        <a href={resume} target="_blank" download>
-          <FancyButton />
-        </a>
-      </div>
-    </div>
+      <HeaderDrawer open={open} handleOpen={handleOpen} />
+    </>
   );
 };
 
---
+++ src/Components/HeaderDrawer.jsx
@@ -0,0 +1,46 @@
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import resume from "../assets/Nameer (React Js + MERN).pdf";
+import { useState } from "react";
+import FancyButton from "./FancyButton";
+
+export default function HeaderDrawer({ open, handleOpen }) {
+  const DrawerList = (
+    <Box sx={{ width: 270 }} role="presentation" onClick={handleOpen}>
+      <div className=" flex flex-col text-black gap-6 p-6 mt-12 flex-1 justify-center font-semibold text-2xl">
+        <div className=" cursor-pointer">
+          <a href="#skills">Skills</a>
+        </div>
+        <div className=" cursor-pointer">
+          {" "}
+          <a href="#experience"> Experience</a>
+        </div>
+        <div className=" cursor-pointer">
+          {" "}
+          <a href="#work">Work</a>
+        </div>
+        <div className=" cursor-pointer">
+          {" "}
+          <a href="#achievements">Achievements</a>
+        </div>
+        <div className=" cursor-pointer">
+          <a href="#contact">Contact</a>
+        </div>
+        <a href={resume} target="_blank" download>
+          <div className="resumeButton2">
+            <FancyButton />
+          </div>
+        </a>
+      </div>
+    </Box>
+  );
+
+  return (
+    <div>
+      <Drawer open={open} onClose={handleOpen}>
+        {DrawerList}
+      </Drawer>
+    </div>
+  );
+}
