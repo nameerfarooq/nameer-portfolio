8e2a6b34b171574e4e1a16ee53a8eab20c767028
Nameerfarooq <nameerfarooquit@gmail.com>
2024-07-08 23:34:53 +0500

    update

--- src/Components/SkillItem.jsx
+++ src/Components/SkillItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SkillItem = ({ name, image }) => {
   return (
-    <div className="skill-item flex flex-col gap-4 p-1 rounded w-28 h-32 sm:w-32 sm:h-40 border-b-8 transition-all ease-in-out duration-300 cursor-pointer hover:bg-black hover:text-white  border-2 border-black items-center justify-center">
+    <div className="skill-item flex flex-col gap-4 p-1 rounded w-24 h-32 sm:w-32 sm:h-40 border-b-8 transition-all ease-in-out duration-300 cursor-pointer hover:bg-black hover:text-white  border-2 border-black items-center justify-center">
       <img className="w-8 h-8 sm:w-24 sm:h-24 object-contain" src={image} alt="" />
       <p className="text-center">{name}</p>
     </div>
