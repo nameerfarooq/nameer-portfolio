90d55571f19c4b832bdf920adecbbb454fa94e0c
Nameerfarooq <nameerfarooquit@gmail.com>
2024-07-13 21:13:27 +0500

    scroll bar added

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,20 @@ import Acheivements from "./Components/Acheivements";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 import ScrollToTop from "./Components/ScrollToTop";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const App = () => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   return (
     <div className="font-sora">
       <Header />
+      <motion.div className="progress-bar" style={{ scaleX }} />
+
       <HeroSection />
       <Skills />
       <Experience />
